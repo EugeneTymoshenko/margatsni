47d6a57ebe0a141252f7fa27ecc2a37b7519990b
EugeneTymoshenko <jack.tymoshenko@gmail.com>
2019-02-21 16:41:44 +0200

    WIP

--- app/api/margatsni/v1/helpers/auth.rb
+++ app/api/margatsni/v1/helpers/auth.rb
@@ -6,19 +6,26 @@ module Margatsni
       module Auth
         def validate_token!
           TokenProvider.valid?(token)
-        # rescue
-        #   error!('Unauthorized', 401)
+        rescue JWT::VerificationError, JWT::DecodeError
+          error!('Unauthorized', 401)
         end
 
-        def authenticate!
-          payload, header = TokenProvider.valid?(token)
+        def authenticate_request!
+          payload, _header = validate_token!
           @current_user = User.find_by(id: payload['user_id'])
         # rescue
         #   error!('Unauthorized', 401)
         end
 
+        def authenticate_user!
+          user = User.authenticate(params[:email], params[:password])
+          return error!('Unauthorized', 401) unless user
+
+          present :token, ::TokenProvider.issue_token(user_id: user.id)
+        end
+
         def current_user
-          @current_user ||= authenticate!
+          @current_user ||= authenticate_user!
         end
 
         def token
