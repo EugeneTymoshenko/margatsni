46751de57c3d29c16c4d98182e2ba1897b67e681
EugeneTymoshenko <jack.tymoshenko@gmail.com>
2019-03-09 22:08:25 +0200

    add posts endpoints

--- app/api/margatsni/v1/endpoints/posts.rb
+++ app/api/margatsni/v1/endpoints/posts.rb
@@ -12,17 +12,24 @@ module Margatsni
 
         namespace :posts do
           desc 'Return list of posts'
+          params do
+            optional :page, type: Integer
+            optional :per_page, type: Integer
+          end
           get do
-            post = Post.all
-            present post, with: Margatsni::V1::Entities::Post
+            posts = Post.all.page(params[:page]).per(params[:per_page])
+            present :page, posts.current_page
+            present :per_page, posts.current_per_page
+            present :posts, posts, with: Margatsni::V1::Entities::Post
           end
 
           desc 'Return a specific post'
           params do
-            requires :id, type: String
+            requires :id, type: Integer, allow_blank: false
           end
           get :id do
             post = Post.find(params[:id])
+
             represent_post(post)
           rescue ActiveRecord::RecordNotFound
             error!('Post not found!', 404)
@@ -34,7 +41,7 @@ module Margatsni
 
           desc 'Create new post'
           params do
-            requires :body, type: String
+            requires :body, type: String, length: 500
           end
           post :new do
             post = current_user.posts.create(declared(params))
@@ -44,21 +51,24 @@ module Margatsni
 
           desc 'Update a post'
           params do
-            requires :id, type: String
-            requires :body, type: String
+            requires :id, type: Integer
+            requires :body, type: String, length: 500
           end
           put :id do
-            current_user.posts.find(params[:id]).update(body: params[:body])
+            post = current_user.posts.find(params[:id])
+            post.update(body: params[:body])
+
+            represent_post(post)
           rescue ActiveRecord::RecordNotFound
             error!('Post not found!', 404)
           end
 
           desc 'Delete a post'
           params do
-            requires :id, type: String
+            requires :id, type: Integer, allow_blank: false
           end
           delete :id do
-            current_user.posts.find(params[:id]).destroy
+            present :status, !current_user.posts.find(params[:id]).destroy.nil?
           rescue ActiveRecord::RecordNotFound
             error!('Post not found!', 404)
           end
