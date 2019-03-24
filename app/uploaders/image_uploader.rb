0740e358de776e9275318025a8376515ba4a17c7
EugeneTymoshenko <jack.tymoshenko@gmail.com>
2019-03-24 21:49:52 +0200

    fix heroku images

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -7,6 +7,10 @@ class ImageUploader < CarrierWave::Uploader::Base
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
+  def cache_dir
+    "#{Rails.root}/tmp/uploads"
+  end
+
   def size_range
     0..5.megabytes
   end
