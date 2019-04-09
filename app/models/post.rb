55c6b1d1734a53eee1083a4da0cd753ce5169250
EugeneTymoshenko <jack.tymoshenko@gmail.com>
2019-04-09 21:40:33 +0300

    add habtm relation, ability to create and update tags

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,5 +6,34 @@ class Post < ApplicationRecord
   has_many :likes, as: :likeable, dependent: :destroy
   has_many :comments, as: :commentable, dependent: :destroy
 
+  has_and_belongs_to_many :tags, dependent: :destroy
+
   accepts_nested_attributes_for :image
+
+  after_create :create_tags
+
+  before_update :update_tags
+
+  def update_tags
+    post = Post.find_by(id: id)
+    post.tags.clear
+    extract_name_tags.each do |name|
+      tag = Tag.find_or_create_by(name: name)
+      post.tags << tag
+    end
+  end
+
+  def create_tags
+    post = Post.find_by(id: id)
+    extract_name_tags.each do |name|
+      tag = Tag.find_or_create_by(name: name)
+      post.tags << tag
+    end
+  end
+
+  private
+
+  def extract_name_tags
+    body.scan(/#\w+/).uniq.map { |name| name.downcase.delete('#') }
+  end
 end
