5e5a5448a1eebbb5a37cb2c57715adc22a0594be
EugeneTymoshenko <jack.tymoshenko@gmail.com>
2019-04-12 01:49:50 +0300

    [comments] add separated endpoint for creating nested comment add pagination for list of comments refactor code style

--- app/api/margatsni/v1/endpoints/comments.rb
+++ app/api/margatsni/v1/endpoints/comments.rb
@@ -5,16 +5,18 @@ module Margatsni
     module Endpoints
       class Comments < Margatsni::V1::BaseV1
         helpers do
+          attr_reader :post, :comment
+
           def represent_comment(comment)
-            present :comment, comment, with: Margatsni::V1::Entities::Comment
+            present :comment, comment, with: Margatsni::V1::Entities::Comment, except: %i[nested_comments]
           end
 
-          def post
+          def find_post!
             @post ||= Post.find_by(id: params[:post_id])
             @post || error!('Post not found', 404)
           end
 
-          def comment(scope = {})
+          def find_comment!(scope = {})
             scope[:id] = params[:comment_id]
             @comment ||= Comment.find_by(scope)
             @comment || error!('Comment not found', 404)
@@ -22,24 +24,34 @@ module Margatsni
         end
 
         namespace 'posts/:post_id' do
+          before do
+            find_post!
+          end
+
           resources :comments do
             desc 'Get all comments for a specific post'
+            params do
+              optional :page, type: Integer
+              optional :per_page, type: Integer
+            end
             get do
-              present :comments, post.comments, with: Margatsni::V1::Entities::Comments
+              comments = post.comments.page(params[:page]).per(params[:per_page])
+
+              present :page, comments.current_page
+              present :per_page, comments.current_per_page
+              present :comments, comments, with: Margatsni::V1::Entities::Comment
             end
 
             before do
               authenticate_request!
             end
 
-            desc 'Create a comment'
+            desc 'Create a comment for post'
             params do
-              requires :body, type: String, allow_blank: false
-              optional :comment_id, type: Integer, allow_blank: false
+              requires :body, type: String, allow_blank: false, length: 300
             end
             post do
-              owner = params[:comment_id] ? comment : post
-              comment = owner.comments.build(body: params[:body], user_id: current_user.id)
+              comment = post.comments.build(body: params[:body], user: current_user)
               error!(comment.errors.full_messages) unless comment.save
 
               represent_comment(comment)
@@ -48,17 +60,35 @@ module Margatsni
             route_param :comment_id do
               desc 'Update a comment'
               params do
-                requires :body, type: String, allow_blank: false, length: 200
+                requires :body, type: String, allow_blank: false, length: 300
               end
               put do
-                comment(user_id: current_user.id).update(declared(params))
+                find_comment!(user: current_user)
+                comment.update(declared(params))
 
                 represent_comment(comment)
               end
 
               desc 'Delete a comment'
               delete do
-                present :status, comment(user_id: current_user.id).destroy.present?
+                find_comment!(user: current_user)
+
+                present :status, comment.destroy.present?
+              end
+
+              before do
+                find_comment!
+              end
+
+              desc 'Create a nested comment'
+              params do
+                requires :body, type: String, allow_blank: false, length: 300
+              end
+              post do
+                nested_comment = comment.comments.build(body: params[:body], user: current_user)
+                error!(nested_comment.errors.full_messages) unless nested_comment.save
+
+                represent_comment(nested_comment)
               end
             end
           end
