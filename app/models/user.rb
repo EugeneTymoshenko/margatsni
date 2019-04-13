235bb012b16df1cc881f7c6a7cc1d69147167b57
Victor Skyba <victor.skyba.v@gmail.com>
2019-04-13 23:12:55 +0300

    add search user scope

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,7 +14,7 @@ class User < ApplicationRecord
   has_many :followers, through: :follower_users, source: :user, class_name: 'User'
   has_many :following, through: :following_users, source: :follower, class_name: 'User'
 
-  scope :sea, -> { where("username LIKE ?", "%#{params[:username]}%") }
+  scope :search_by_username, -> (query) { where('username LIKE ?', "%#{query}%") }
 
   validates :username, presence: true, uniqueness: { case_sensitive: false }
   validates :password, length: { minimum: 6 }, on: :create
